4d1733934cd4beb89b0e93ef29da45f6fcd17d6e
Nishant Chauhan <mailxnishant@gmail.com>
2025-04-10 14:55:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ config({
 });
 
 const app = express();
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: "https://doc-gpt-xi.vercel.app", credentials: true }));
 app.use(express.json());
 
 connectDB();
